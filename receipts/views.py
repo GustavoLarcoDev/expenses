1d0f5e89fe5f8cfa043bba42cedd659ca1f190c4
Gustavo Larco <gustavo.larcoj@gmail.com>
2024-08-15 10:42:08 -0500

    Feature 11 complete

--- receipts/views.py
+++ receipts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from receipts.models import Receipt
 from django.contrib.auth.decorators import login_required
+from receipts.forms import ReceiptForm
 
 
 @login_required
@@ -10,3 +11,20 @@ def receipt_list(request):
         "receipts_object": receipts
     }
     return render(request, "receipts/ReceiptList.html", context)
+
+
+@login_required
+def create_receipt(request):
+    if request.method == "POST":
+        form = ReceiptForm(request.POST)
+        if form.is_valid():
+            Receipt = form.save(commit=False)
+            Receipt.purchaser = request.user
+            Receipt.save()
+            return redirect("home")
+    else:
+        form = ReceiptForm()
+    context = {
+        "form": form
+    }
+    return render(request, "receipts/CreateReceipt.html", context)
